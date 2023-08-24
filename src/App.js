fd6e337b25fe75cd42c5c1db7c43f8408c585f6e
Faisallm <faisallawan1997@gmail.com>
2023-08-24 16:01:49 -0700

    Gear 1st

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-
-import Todo from "./components/Todo";
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
-      <Todo text="Faisal Muhammad" />
-    </div>
+    <Layout>
+      {/* pages to be displayed depending on url */}
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
