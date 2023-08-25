ab3e12d8aea9eef48a94b73535faadd4c1f563c4
Faisallm <faisallawan1997@gmail.com>
2023-08-24 19:04:09 -0700

    gear 2nd

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,3 +1,67 @@
-function NewMeetupForm() {}
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+import { useRef } from "react";
+
+function NewMeetupForm(props) {
+  // will give us access to input element...
+  // through ref object.
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetUpData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription,
+    }
+
+    props.onAddMeetUp(meetUpData)
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" ref={titleInputRef} required />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" id="image" ref={imageInputRef} required />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" ref={addressInputRef} required />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            ref={descriptionInputRef}
+            rows="5"
+          ></textarea>
+        </div>
+
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
 
 export default NewMeetupForm;
